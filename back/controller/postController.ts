14ee354cdae3ecaa243a39bd4c8746f0388a649a
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-16 18:14:16 +0330

    methods for comminucate api and client

--- back/controller/postController.ts
+++ back/controller/postController.ts
@@ -12,5 +12,33 @@ export const getPosts: ControllerType = (req, res, next) => {
       },
       date: new Date(),
     },
+    {
+      _id: "2",
+      title: "Post Title 2",
+      content: "Post Content 2",
+      imageUrl: "images/image-not-available.png",
+      creator: {
+        name: "Mojeeto",
+      },
+      date: new Date(),
+    },
   ]);
+};
+
+export const newPost: ControllerType = (req, res, next) => {
+  const { title, content } = req.body;
+  console.log(req.body);
+  res.status(201).json({
+    message: "Successful",
+    data: {
+      _id: "3",
+      title,
+      content,
+      imageUrl: "images/image-not-available.png",
+      creator: {
+        name: "Mojeeto",
+      },
+      date: new Date(),
+    },
+  });
 };
--- back/routes/postRouter.ts
+++ back/routes/postRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getPosts } from "../controller/postController";
+import { getPosts, newPost } from "../controller/postController";
 
 const postRouter = Router();
 
 postRouter.get("/posts", getPosts);
+postRouter.post("/post", newPost);
 
 export default postRouter;
