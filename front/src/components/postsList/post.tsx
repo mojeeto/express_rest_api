7b00b60122a2dada7c6d6fc5b7733c7a8d0f84fe
Mojeeto <mojtaba01nouri@gmail.com>
2023-11-16 20:48:59 +0330

    showing post image

--- front/src/components/postsList/post.tsx
+++ front/src/components/postsList/post.tsx
@@ -5,7 +5,11 @@ import { Link } from "react-router-dom";
 
 const Post: React.FC<PostProps> = ({ post }) => {
   return (
-    <Card className="max-w-sm" horizontal>
+    <Card
+      className="max-w-sm"
+      imgSrc={`http://localhost:8080/images/${post.imagePath}`}
+      horizontal
+    >
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         {post.title}
       </h5>
